eb97e6c4c20811cdf655528432196c4a5199d383
Matthew VanDyke <48613756+MatthewVandyke@users.noreply.github.com>
2020-10-30 22:09:54 -0700

    Button's update onclick. Newgame clears all buttons

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -6,20 +6,68 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+
+import com.example.tictactoe.databinding.ActivityMainBinding;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ArrayList<View> buttons;
+    private String currPlayer;
+    private ActivityMainBinding binding;
+    private ArrayList<Button> buttons;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
 
+        buttons = new ArrayList<>();
 
-        buttons = ((ConstraintLayout) findViewById(R.id.button_container)).getTouchables();
-        buttons.get(1).getId();
+        buttons.add(binding.button1);
+        buttons.add(binding.button2);
+        buttons.add(binding.button3);
+        buttons.add(binding.button4);
+        buttons.add(binding.button5);
+        buttons.add(binding.button6);
+        buttons.add(binding.button7);
+        buttons.add(binding.button8);
+        buttons.add(binding.button9);
+        currPlayer = "X";
+
+    }
+
+    public void resetGame(View view) {
+        for (Button button : buttons){
+            button.setText("");
+        }
+        updateCurrPlayer("X");
+    }
+
+    private void updateCurrPlayer(String name) {
+        currPlayer = name;
+        if (name.equals("X")){
+            binding.textView.setText(R.string.playerX);
+        }
+        else {
+            binding.textView.setText(R.string.playerO);
+        }
+    }
+
+    public void onButtonClick(View view) {
+        Button button = (Button) view;
+        if(button.getText().length() == 0) {
+            button.setText(currPlayer);
+            if (currPlayer.equals("X")) {
+                updateCurrPlayer("O");
+            } else {
+                updateCurrPlayer("X");
+            }
+        }
     }
 }
