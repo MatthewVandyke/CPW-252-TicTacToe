8442d2c38cdfcde88cb93363a43f903cd26f98f7
Matthew VanDyke <48613756+MatthewVandyke@users.noreply.github.com>
2020-11-12 20:10:29 -0800

    Working TicTacToe

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -17,7 +17,8 @@ public class MainActivity extends AppCompatActivity {
 
     private String currPlayer;
     private ActivityMainBinding binding;
-    private ArrayList<Button> buttons;
+    private Button[][] buttons;
+    private boolean gameWon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,26 +28,21 @@ public class MainActivity extends AppCompatActivity {
         View view = binding.getRoot();
         setContentView(view);
 
-        buttons = new ArrayList<>();
-
-        buttons.add(binding.button1);
-        buttons.add(binding.button2);
-        buttons.add(binding.button3);
-        buttons.add(binding.button4);
-        buttons.add(binding.button5);
-        buttons.add(binding.button6);
-        buttons.add(binding.button7);
-        buttons.add(binding.button8);
-        buttons.add(binding.button9);
+        buttons = new Button[][]{{binding.button1, binding.button2, binding.button3},
+                                 {binding.button4, binding.button5, binding.button6},
+                                 {binding.button7, binding.button8, binding.button9}};
         currPlayer = "X";
 
     }
 
     public void resetGame(View view) {
-        for (Button button : buttons){
-            button.setText("");
+        for (Button[] row : buttons){
+            for (Button button : row){
+                button.setText("");
+            }
         }
         updateCurrPlayer("X");
+        gameWon = false;
     }
 
     private void updateCurrPlayer(String name) {
@@ -60,14 +56,58 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onButtonClick(View view) {
+        if (gameWon)
+            return;
         Button button = (Button) view;
         if(button.getText().length() == 0) {
             button.setText(currPlayer);
+            if(checkWin(currPlayer)) {
+                gameWon = true;
+                binding.textView.setText("Player " + currPlayer + " Wins!");
+                return;
+            }
+
             if (currPlayer.equals("X")) {
                 updateCurrPlayer("O");
             } else {
                 updateCurrPlayer("X");
             }
         }
+    }
+
+    private boolean checkWin(String player) {
+
+        //Check rows
+        for (Button[] row : buttons){
+            if (row[0].getText().equals(player)
+            && row[1].getText().equals(player)
+            && row[2].getText().equals(player)) {
+                return true;
+            }
+        }
+
+        //Check cols
+        for (int i = 0; i < 3; i++) {
+            if (buttons[0][i].getText().equals(player)
+            && buttons[1][i].getText().equals(player)
+            && buttons[2][i].getText().equals(player)) {
+                return true;
+            }
+        }
+
+        //Check X
+        if (buttons[0][0].getText().equals(player)
+        && buttons[1][1].getText().equals(player)
+        && buttons[2][2].getText().equals(player)) {
+            return true;
+        }
+
+        if (buttons[0][2].getText().equals(player)
+        && buttons[1][1].getText().equals(player)
+        && buttons[2][0].getText().equals(player)) {
+            return true;
+        }
+
+        return false;
     }
 }
